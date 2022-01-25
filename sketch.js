6c18dfebef492c04945cd483bf4aa92be4f741d1
AryaDude <76018122+AryaDude@users.noreply.github.com>
2022-01-25 18:01:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -118,7 +118,7 @@ async function getBackgroundImg(){
         bg = "sprites/bg1.png";
     }
     else{
-        bg = "sprites/bg2.jpg";
+        bg = "sprites/bg1.png";
     }
 
     backgroundImg = loadImage(bg);
